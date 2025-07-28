a0db43b65b807421db95e5120cd5c952d818292c
projectsky5 <mackvej@gmail.com>
2025-07-28 17:19:07 +0300

    Initial commit

---
+++ src/main/java/com/projectsky/exception_handler_starter/ExceptionHandlerStarterApplication.java
@@ -0,0 +1,13 @@
+package com.projectsky.exception_handler_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionHandlerStarterApplication.class, args);
+    }
+
+}
